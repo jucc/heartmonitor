72f537200eb93059f46c8bbf8c7c95f693113028
Juliana Cavalcanti <jucorrea.mail@gmail.com>
2017-03-31 13:01:52 -0300

    adding to a function

--- hrm.py
+++ hrm.py
@@ -1,7 +1,6 @@
 import bluepy.btle as btle
+from datetime import datetime
 import time
-
-polarmac = "00:22:D0:85:88:8E"
 
 class HRMMNotificationsDelegate(btle.DefaultDelegate):
     """
@@ -10,38 +9,42 @@ class HRMMNotificationsDelegate(btle.DefaultDelegate):
     """
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
+        self.t0 = time.time()
 
     def handleNotification(self, cHandle, data):
         bpm = ord(data[1])
-        print "[%.2f]"%(time.time()-t0), bpm
+        print "[%.2f]"%(time.time()-self.t0), bpm
 
-# MAIN
 
-hrm = 0
-try:
-    #connect to device
-    hrm = btle.Peripheral(polarmac)
-    print "connected"
+def hrm(mac):
+    try:
+        hrm = btle.Peripheral(mac)
+        print "Connected to device"
 
-    # read fixed ble specs for hrm service and hrmm characteristic
-    hrmid = btle.AssignedNumbers.heart_rate
-    hrmmid = btle.AssignedNumbers.heart_rate_measurement
-    cccid = btle.AssignedNumbers.client_characteristic_configuration
+        # read fixed ble specs for hrm service and hrmm characteristic
+        hrmid = btle.AssignedNumbers.heart_rate
+        hrmmid = btle.AssignedNumbers.heart_rate_measurement
+        cccid = btle.AssignedNumbers.client_characteristic_configuration
 
-    # query device for the service (hrm), characteristics and descriptors
-    serv = hrm.getServiceByUUID(hrmid)
-    chars = serv.getCharacteristics(hrmmid)[0]
-    desc = hrm.getDescriptors(serv.hndStart, serv.hndEnd)
-    d = [d for d in desc if d.uuid==cccid][0]
+        # query device for the service (hrm), characteristics and descriptors
+        serv = hrm.getServiceByUUID(hrmid)
+        chars = serv.getCharacteristics(hrmmid)[0]
+        desc = hrm.getDescriptors(serv.hndStart, serv.hndEnd)
+        d = [d for d in desc if d.uuid==cccid][0]
 
-    # register handler to receive notifications
-    hrm.writeCharacteristic(d.handle, '\1\0')
-    hrm.setDelegate(HRMMNotificationsDelegate())
+        # register handler to receive notifications
+        hrm.writeCharacteristic(d.handle, '\1\0')
+        hrm.setDelegate(HRMMNotificationsDelegate())
 
-    # START GOING
-    t0 = time.time()
-    while(True):
-        hrm.waitForNotifications(1.5)
+        # START GOING
+        while(True):
+            hrm.waitForNotifications(1.5)
 
-finally:
-    hrm.disconnect()
+    finally:
+        hrm.disconnect()
+
+
+if __name__ == '__main__':
+
+    polarmac = "00:22:D0:85:88:8E"
+    hrm(polarmac)
